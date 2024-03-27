103c2ba538b162aa16723348df9692392ab0228f
Benjamin Ott <benjaminott933@gmail.com>
2024-03-27 16:08:10 +0100

    update

---
+++ DigitalizeAware/Views/Home/Home.swift
@@ -0,0 +1,18 @@
+//
+//  Home.swift
+//  DigitalizeAware
+//
+//  Created by Benjamin Ott on 27.03.24.
+//
+
+import SwiftUI
+
+struct Home: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+#Preview {
+    Home()
+}
